9c7c8cb6c3145cb7289bcbe906eb5112cf03a581
Nathan <nathannxx@proton.me>
2026-06-22 16:45:23 +0200

    tmp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,10 +8,11 @@ MIUICAMERA_PATH := device/xiaomi/tanzanite-miuicamera
 CAMERA_PACKAGE_NAME := com.android.camera
 
 # Properties
-TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(MIUICAMERA_PATH)/system_ext.prop
 
 # SEPolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/private
 
 # Inherit from the proprietary version
 include vendor/xiaomi/tanzanite-miuicamera/BoardConfigVendor.mk
